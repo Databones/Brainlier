644b5923b7e3a40974c501e9b0050c372d57573f
Databones <130965239+Databones@users.noreply.github.com>
2023-11-06 14:51:51 -0700

    Update Brainlier.user.js

--- Brainlier.user.js
+++ Brainlier.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Brainlier
 // @namespace    https://discord.gg/8sKGWD7BTc
-// @version      2.1
+// @version      2.2
 // @description  Unlocks Brainly's questions and textbook answers.
 // @author       Databones
 // @icon         https://raw.githubusercontent.com/Databones/Brainlier/main/IER.png
@@ -19,4 +19,4 @@
 // @run-at       document-start
 // ==/UserScript==
 
-localStorage.setItem("textbookMetering/unlogged/answerViews","-9999");const itemsToRemove=["social-qa/telemetry","simple-funnel-name","flexible-funnel-cycle-data",];itemsToRemove.forEach(e=>{localStorage.getItem(e)&&localStorage.removeItem(e)});const versionFileURL="https://raw.githubusercontent.com/Databones/Brainlier/main/version",redirectURL="https://github.com/Databones/Brainlier/raw/main/Brainlier.user.js";function fetchRemoteVersion(){fetch("https://raw.githubusercontent.com/Databones/Brainlier/main/version").then(e=>e.text()).then(e=>{let t=GM_info.script.version;e.trim()!==t.trim()&&(window.location.href="https://github.com/Databones/Brainlier/raw/main/Brainlier.user.js")})}
+localStorage.setItem("textbookMetering/unlogged/answerViews","-9999");const itemsToRemove=["social-qa/telemetry","simple-funnel-name","flexible-funnel-cycle-data"];itemsToRemove.forEach((e=>{localStorage.getItem(e)&&localStorage.removeItem(e)}));const versionFileURL="https://raw.githubusercontent.com/Databones/Brainlier/main/version",redirectURL="https://github.com/Databones/Brainlier/raw/main/Brainlier.user.js";function fetchRemoteVersion(){fetch(versionFileURL).then((e=>e.text())).then((e=>{const t=GM_info.script.version;e.trim()!==t.trim()&&(window.location.href=redirectURL)}))}"undefined"!=typeof GM_info&&GM_info.script&&fetchRemoteVersion();
